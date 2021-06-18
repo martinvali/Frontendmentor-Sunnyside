c800f55a462e89f93ae2bbcb731a2d7830713336
Martin Väli <martinvali12@gmail.com>
2021-06-18 21:25:54 +0300

    Partly do red img section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const mobileHamburgerMenu = document.querySelector(".mobile-burger-menu");
 const navigationLinks = document.querySelector(".nav-links");
-
 mobileHamburgerMenu.addEventListener("click", () => {
   navigationLinks.classList.toggle("nav-links-open");
 });
