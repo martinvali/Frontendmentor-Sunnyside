5e70da3e1f607efa81239535555568bce07f5d8c
Martin Väli <martinvali12@gmail.com>
2021-06-18 11:30:06 +0300

    Add mobile html and css

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const mobileHamburgerMenu = document.querySelector(".mobile-burger-menu");
+const navigationLinks = document.querySelector(".nav-links");
+
+mobileHamburgerMenu.addEventListener("click", () => {
+  navigationLinks.classList.toggle("nav-links-open");
+});
